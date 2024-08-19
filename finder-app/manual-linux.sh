a49bb671fb31850c5aa6552bf305735fdc714935
AhmedElkazzaz94 <ensanportsaid@gmail.com>
2024-08-19 17:37:59 +0200

    Setting setuid bit for BusyBox

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -72,6 +72,9 @@ make defconfig
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
+#Setting setuid bit for BusyBox
+sudo chmod +s ${OUTDIR}/rootfs/bin/busybox
+
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
